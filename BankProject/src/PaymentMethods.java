5293db586bc135aa00c0499cf76b1dc311bef1cf
haaktar <haaktar@gmail.com>
2022-08-17 22:42:45 -0400

    update

--- BankProject/src/PaymentMethods.java
+++ BankProject/src/PaymentMethods.java
@@ -3,8 +3,8 @@ import java.time.format.DateTimeFormatter;
 public class PaymentMethods extends Customer {
 
     double balance;
-    String username;
-    String userPassword;
+    String username = "hakan";
+    String userPassword = "123";
 
 
     public PaymentMethods(String name, String lastName) {
@@ -21,17 +21,44 @@ public class PaymentMethods extends Customer {
         // System.out.println();
         System.out.println(formatDateTime);
         System.out.println();
+
+    }
+
+    public void loginScreen() {
+
         System.out.println("Please Enter Your Username");
         System.out.println("Username : ");
         String userNameScanner = scanner.next();
-
-        username = userNameScanner;
-
         System.out.println("Please Enter Your Password");
         System.out.println("Password: ");
         String userPasswordScanner = scanner.next();
-        userPassword = userPasswordScanner;
 
+        if ((username.equalsIgnoreCase(userNameScanner)) && (userPassword.equalsIgnoreCase(userPasswordScanner))) {
+
+            System.out.println("Log in Successful");
+
+
+        } else {
+
+            for (; ; ) {
+                System.err.println("Wrong Username or Password");
+                System.out.println("Please Enter Your Username");
+                System.out.println("Username : ");
+                String userNameScanner2 = scanner.next();
+                System.out.println("Please Enter Your Password");
+                System.out.println("Password: ");
+                String userPasswordScanner2 = scanner.next();
+
+                if ((username.equalsIgnoreCase(userNameScanner2)) && (userPassword.equalsIgnoreCase(userPasswordScanner2))) {
+
+                    System.out.println("Log in Successful");
+
+                    System.exit(0);
+
+                }
+
+            }
+        }
     }
 
     @Override
@@ -52,12 +79,10 @@ public class PaymentMethods extends Customer {
     public void withdrawMoney(double withdraw) {
 
         this.withdraw = withdraw;
-
         balance -= withdraw;
 
         if (balance < withdraw) {
             System.out.println("Withdraw  amount is  less then balance");
-
             System.exit(0);
         }
 
@@ -65,7 +90,6 @@ public class PaymentMethods extends Customer {
 
     @Override
     public void transferMoney(double deposit, double withdraw) {
-
         this.deposit = deposit;
         this.withdraw = withdraw;
 
